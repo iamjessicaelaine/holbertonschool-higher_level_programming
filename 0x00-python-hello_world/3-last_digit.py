684008daea575c571eea1333d38f683f9a71ca53
iamjessicaelaine <jessicaelaineloyd@gmail.com>
2022-05-07 22:15:46 +0000

    another attempt to passs checker on error type

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print("Last digit of {:d} is ".format(number), end='')
+print("Last digit of {} is ".format(number), end='')
 num_str = str(number)
 negornah = num_str[0]
-lastdig = num_str[-1]
+lastdig = int(num_str[-1])
 if negornah == '-':
     print("-{} and is less than 6 and not 0".format(lastdig))
 else:
     print("{} ".format(lastdig), end='')
-    if lastdig > '5':
+    if lastdig > 5:
         print("and is greater than 5")
-    if((lastdig < '6') and (lastdig != '0')):
+    if((lastdig < 6) and (lastdig != 0)):
         print("and is less than 6 and not 0")
-    if lastdig == '0':
+    if lastdig == 0:
         print("and is 0")
