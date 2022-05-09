4a0d20b59fdf659d8d0a416d1ea54bbf31a09055
iamjessicaelaine <95127791+iamjessicaelaine@users.noreply.github.com>
2022-05-08 19:32:16 -0500

    Update 2-replace_in_list.py

--- 0x02-python-data_structures/2-replace_in_list.py
+++ 0x02-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     listlen = len(my_list)
-    if idx < 0 or idx > listlen:
+    if idx <= 0 or idx >= listlen:
         return my_list
     for i in range(listlen):
         if i == idx:
