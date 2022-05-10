a4b89faec5db23f053175bf4b7bf5ed9ce40a9ca
iamjessicaelaine <95127791+iamjessicaelaine@users.noreply.github.com>
2022-05-10 02:57:08 -0500

    Update 0-square_matrix_simple.py

--- 0x03-python-more_data_structures/0-square_matrix_simple.py
+++ 0x03-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    # creates an empty matrix
     newmatrix = [[0 for x in range(len(matrix))]
                  for i in range(len(matrix[0]))]
-    # create an empty matrix
-    newmatrix = [[0 for x in range(len(matrix))
-                    for i in range(len(matrix[0]))]]
     # 1st loop access rows, 2nd loop the columns
     for x in range(len(matrix)):
         for i in range(len(matrix[0])):
