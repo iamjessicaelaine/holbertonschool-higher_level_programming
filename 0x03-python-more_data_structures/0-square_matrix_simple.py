654d1a9d73d26f0e4d9d69789e26699edce8e10e
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-05-10 00:44:37 -0700

    update to code for task 0 creating empty matrix

--- 0x03-python-more_data_structures/0-square_matrix_simple.py
+++ 0x03-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    # create an empty matrix
     newmatrix = [[0 for x in range(len(matrix))]
                  for i in range(len(matrix[0]))]
-    # 1st loop access rows, 2nd loop the columns
     for x in range(len(matrix)):
         for i in range(len(matrix[0])):
             newmatrix[x][i] = matrix[x][i] ** 2
