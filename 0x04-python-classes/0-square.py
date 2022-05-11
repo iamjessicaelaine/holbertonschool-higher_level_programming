be9552a0280995d142baf3ebac12bef71bc55474
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-05-10 22:00:50 -0700

    code for task 1 project 0x04

---
+++ 0x04-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Square:
+    """defines a square by size attribute"""
+
+    def __init__(self, size):
+        """create new field/private instance size"""
+        self.__size = size
