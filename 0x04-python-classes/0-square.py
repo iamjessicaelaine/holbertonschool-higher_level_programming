8d237e070f34bcdb568bb4d5fb6423720f518123
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-05-10 22:13:28 -0700

    update to documentation on code for task 0 project 0x00

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+""" writes a class square"""
+
+
 class Square:
     """defines a square by size attribute"""
-
     def __init__(self, size):
         """create new field/private instance size"""
         self.__size = size
