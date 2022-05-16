20811a5771e9e9265350349d0a55e7037c94eb02
iamjessicaelaine <95127791+iamjessicaelaine@users.noreply.github.com>
2022-05-16 00:44:09 -0500

    Update 6-print_matrix_integer.py

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for number in range(0, len(row)):
-            print("{:d}".format(number), end=' ')
+            print("{:d}".format(row[number]), end=' ')
         print("".format('\n'))
