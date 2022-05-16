b91643f2659a813c9d409537ec2b7f3406d13d70
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-05-15 21:07:16 -0700

    change to code for task 6

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for number in row:
-            print("{:d}".format(number), end=' ')
+            print("{}".format(number), end=' ')
         print("".format('\n'))
