8cf15f3b10f8af1bda8e710d5f5345043a07ef14
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-05-16 21:32:09 -0700

    code for task 0 project 0x06

---
+++ 0x06-python-more_classes/0-rectangle.py
@@ -0,0 +1,45 @@
+##!/usr/bin/python3
+"""writes a class Rectangle"""
+
+
+class Rectangle:
+    """an attempt to model a rectangle"""
+    def __init__(self, width=0, height=0):  # instantiation w/ optional w & h
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width  # create private instance attribute
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height  # create private instance attribute
+
+    @property
+    def width(self):  # method used to get width
+        return self.__width
+
+    @width.setter
+    def width(self, value):  # method used to change width value
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):  # method used to retrieve height attributer
+        return self.__height
+
+    @height.setter
+    def height(self, value):  # method used to change height value
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
