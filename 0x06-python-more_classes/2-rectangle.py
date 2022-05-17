257507f28b0ddd4bd617e02190d9f1a7226c7c5a
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-05-16 22:34:07 -0700

    code for task 3 and comment correction task 2 code

--- 0x06-python-more_classes/2-rectangle.py
+++ 0x06-python-more_classes/2-rectangle.py
@@ -53,7 +53,8 @@ class Rectangle:
         else:
             return ((self.__width + self.__height) * 2)
 
-    def __str__(self):  # return string representation of rct instance
+    # defines what happens when you pass instance of the class to str()
+    def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
         else:
