0dce331fbe9cac24fdb9d1be2b8e757ef56c4f7d
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-05-17 20:42:44 -0700

    hopefully this is simple enough for checker task 0 project 0x03

--- 0x03-python-more_data_structures/0-square_matrix_simple.py
+++ 0x03-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    # creates an empty matrix
-    newmatrix = [[0 for x in range(len(matrix))]
-                 for i in range(len(matrix[0]))]
-    # 1st loop access rows, 2nd loop the columns
-    for x in range(len(matrix)):
-        for i in range(len(matrix[0])):
-            newmatrix[x][i] = matrix[x][i] ** 2
-    return newmatrix
+    return [list(map(lambda x: x**2, elem)) for elem in matrix]
