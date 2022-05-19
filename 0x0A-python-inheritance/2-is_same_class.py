15274049d16276a44248e497ade7d2e6dcfa2e6f
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-05-18 17:51:17 -0700

    switch isinstance to type for EXACT matching reqs

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,4 +4,7 @@
 
 def is_same_class(obj, a_class):
     """returns true if obj arg is instance of a_class arg"""
-    return(isinstance(obj, a_class))
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
