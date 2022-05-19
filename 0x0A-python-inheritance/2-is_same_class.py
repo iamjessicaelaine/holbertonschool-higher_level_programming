86e1cbec44fc3429837c11d6daa2e0eb03719bf1
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-05-18 17:31:55 -0700

    code files for task 2 project 0x07

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""function  identifies if obj. is exactly an instance of a specifc class"""
+
+
+def is_same_class(obj, a_class):
+    """returns true if obj arg is instance of a_class arg"""
+    return(isinstance(obj, a_class))
