0367dce42c0908f2c1b895bdfb5b4e7a463c4c19
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-05-18 19:32:21 -0700

    code for task 5 project 0x07

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""write an empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """defines an empty class"""
+    def __init__(self):
+        """instantiation of a empty class"""
+        pass
