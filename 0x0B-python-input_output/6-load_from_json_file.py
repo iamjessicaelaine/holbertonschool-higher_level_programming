6afd02cc6d751e546f06e6e86697be7500c7d19d
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-05-20 20:39:21 -0700

    another switch back to loads for code in task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,7 +7,8 @@ import json
 
 def load_from_json_file(filename):
     """creates an object from filename, a json file"""
-    with open(filename, 'r', encoding="utf-8") as j:
+    with open(filename, encoding="utf-8") as j:
         # file must actually be read after it's opened w/ read permission
         redfile = j.read()
         obj = json.loads(redfile)
+        return obj
