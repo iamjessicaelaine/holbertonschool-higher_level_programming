ebde5a142a9f31c5076bd89eef6405792d225ad4
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-05-20 21:44:32 -0700

    code for task 7 project 0x08

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,12 +5,13 @@
 import sys  # module needed to retrieve cmd line args
 import json  # importing just in case following imports need it
 
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_frm_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
     # load the arguments or create an empty list
-    pylist = load_frm_json_file("add_item.json")
+    pylist = load_from_json_file("add_item.json")
 except(TypeError, FileNotFoundError):
     pylist = []  # if load function fails, create empty list
 
