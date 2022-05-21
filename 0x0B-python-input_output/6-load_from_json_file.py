3002c4290ff876c9af0d77f41eef7cf072f0bde9
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-05-20 20:27:29 -0700

    from load to loads hopefully makes checker more happy

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,4 +8,4 @@ import json
 def load_from_json_file(filename):
     """creates an object from filename, a json file"""
     with open(filename, 'r', encoding="utf-8") as j:
-        obj = json.load(j)
+        obj = json.loads(j)
