c90c3931067c61d017b7c7b871c771efe6bbf4e8
iamjessicaelaine <95127791+iamjessicaelaine@users.noreply.github.com>
2022-05-26 13:31:37 -0500

    adding validation methods to base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,3 +14,17 @@ class Base:
             # give self.id a unique __nb_objects in each instance
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def w_h_validate(self, name, value):
+        """method to validate width & height"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+
+    def x_y_validate(self, name, value):
+        """method to validate x & y"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value < 0:
+            raise ValueError("{} must be >= 0".format(name))
