4578521a8cb04207362d2bff0f67981042676af1
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-05-30 17:25:05 -0700

    finally figured out why first row wasn't printing right please pass checker

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -8,15 +8,16 @@ def pascal_triangle(n):
     pascslist = []  # empty list to store my sublists/each row
     # append sub-lists into empty list created above
     if rownumber <= 0:
-        return pascslist
+        return []
     else:
         for i in range(rownumber):
             pascslist.append([])
             # then insert 1 into each sublist for future numbers to follow
             pascslist[i].append(1)
             # loop computes math of other numbers based on pascal's math
-            for j in range(1, i):  # starting @ second element in row do math
+            for j in range(1, i):  # starting @ second element in row domath
                 pascslist[i].append(pascslist[i-1][j-1] + pascslist[i-1][j])
+            if n != 0 and i != 0:
                 # append 1 to end of list
                 pascslist[i].append(1)
         return pascslist
