68031c64147342a87e229b5cd3a4963aecc523d5
iamjessicaelaine <95127791+iamjessicaelaine@users.noreply.github.com>
2022-05-31 04:39:12 -0500

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,19 +8,20 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """a model of a rectangle whse inherited from its parent Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, x, y, id)
-        self.__size = size
+        super().__init__(width, height, x, y, id)
+        self.__width = size
+        self.__height = size
         self.__x = x
         self.__y = y
 
     def __str__(self):
         """overiding str method for square class"""
         return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.__x,
-                                                         self.__y, self.__size)
+                                                         self.__y, self.__width)
 
     @property  # getter for size
     def size(self):
-        return self.__size
+        return self.__width
 
     @size.setter  # setter for size
     def size(self, value):
