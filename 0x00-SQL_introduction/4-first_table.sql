64b1f844a3c72c19069c52519d7bf948bfcba206
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-06-08 19:00:04 -0700

    script creating first table in db

---
+++ 0x00-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- script creates a table called `first_table` in the current db
+-- creating a table with fields id and name
+CREATE TABLE IF NOT EXISTS first_table (
+       id INT,
+       name VARCHAR(256)
+);
