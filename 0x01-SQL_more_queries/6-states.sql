d9e5ab8e04d634e71c07b81d93c1658eaefe7e4c
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-06-12 18:41:37 -0700

    script creating table with fields w/ constraints

---
+++ 0x01-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- script creates db `hbtn_0d_usa` and table `states` witthin it on MySQL server
+-- states table has two fields: id and name, id field has specific constraints
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
+( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(256) NOT NULL
+);
