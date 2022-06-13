8b02bade6e7954103c598e82add4bb9e1bb77cae
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-06-12 21:26:55 -0700

    update to table order for join statement

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- lists all cities contained in the database
 -- joining the two tables in DB to give us a specific result
 SELECT cities.id, cities.name, states.name
-FROM states
-INNER JOIN cities
-ON states.id = cities.id
+FROM cities
+INNER JOIN states
+ON cities.id = states.id
 ORDER BY cities.id ASC;
