0aeb145922141c12f03ad67e6bc7b6399c9617b0
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-06-12 21:34:14 -0700

    correction to field name for script showing inner join

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- lists all cities contained in the database
 -- joining the two tables in DB to give us a specific result
 SELECT cities.id, cities.name, states.name
-FROM cities
-INNER JOIN states
-ON cities.id = states.id
+FROM states
+INNER JOIN cities
+ON state.id = cities.states_id
 ORDER BY cities.id ASC;
