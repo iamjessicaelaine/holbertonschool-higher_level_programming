4e4ae2697e6350ae2cb6a282044be2cf4a9c20e1
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-06-12 20:51:11 -0700

    remove USE statment for checker satisfaction

--- 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,8 @@
 -- script lists all the cities of CA that are in db hbtn_0d_usa
 -- because west coast is the best coast
-USE hbtn_0d_usa;
-SELECT id, name FROM cities WHERE state_id
-IN (
+-- this will cause checkr to fail since they use diff db USE hbtn_0d_usa;
+SELECT id, name FROM cities
+WHERE state_id IN (
    SELECT id
    FROM states
    WHERE name = 'California'
