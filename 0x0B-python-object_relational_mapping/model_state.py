6bf6efa80d1365b145661cbae34fb3468cce14f6
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-06-24 19:45:27 -0700

    State class defined and Base class declared

---
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""defines class State and an instance declaration of Base class"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# declaring base: a catalog of clsses & tables in declarative system
+Base = declarative_base()
+
+# mapping the class to the table
+class State(Base):
+    """defining the object state & its respective table"""
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
