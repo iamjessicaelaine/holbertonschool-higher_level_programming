f50e86e0e4f8554c5f6970f593f7de25246d8941
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-07-11 17:40:54 -0700

    script to POST an email using urllib module

---
+++ 0x0C-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+script sends a post request to given url and email & displays decoded
+body of the response
+"""
+from urllib.request import urlopen, Request
+from urlib.parse import urlencode
+from sys import argv
+
+url = argv[1]
+email = argv[2]
+datadict = {'email': email}
+encodeddata = urlencode(datadict)
+utf8data = encodeddata.encode("utf-8")
+
+with urlopen(url, data=utf8data) as response:
+    print(response.body.decode("utf-8"))
