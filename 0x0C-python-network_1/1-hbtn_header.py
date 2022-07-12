d394a1b65b79368f350e2197c673d5bce37fa5d1
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-07-11 17:32:27 -0700

    stopping script from executing on import on task 1

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -3,6 +3,6 @@
 import urllib.request
 from sys import argv
 
-
-with urllib.request.urlopen(argv[1]) as response:
-    print(response.headers['X-Request-Id'])
+if __name__ == "__main__":
+    with urllib.request.urlopen(argv[1]) as response:
+        print(response.headers['X-Request-Id'])
