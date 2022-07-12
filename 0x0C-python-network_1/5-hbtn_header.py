35f7bc63374a32c683ae14808c70105ad1bc74cb
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-07-11 19:58:35 -0700

    script for task 5 project 0x0C

---
+++ 0x0C-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""script request to given url and displays value of variable X-Request-ID"""
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    givenurl = argv[1]
+    response = requests.get(givenurl)
+    print(response.headers['X-Request-ID'])
