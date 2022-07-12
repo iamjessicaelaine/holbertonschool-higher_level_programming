ec5daf370b563f68897a75ffb6d70f69f38ca2e1
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-07-11 19:49:48 -0700

    accessing text to get correct type for response obj

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ import requests
 if __name__ == '__main__':
     response = requests.get('https://intranet.hbtn.io/status')
     print('Body response:')
-    print('\t- type: {}'.format(type(response)))
+    print('\t- type: {}'.format(type(response.text)))
     print('\t- content: {}'.format(response.text))
