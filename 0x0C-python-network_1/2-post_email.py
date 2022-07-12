8116096aaa026d79f5a60e72e84e2baf67258175
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-07-11 17:49:48 -0700

    typo correction to urllib import and needed to read response obj to get body

--- 0x0C-python-network_1/2-post_email.py
+++ 0x0C-python-network_1/2-post_email.py
@@ -4,8 +4,9 @@ script sends a post request to given url and email & displays decoded
 body of the response
 """
 from urllib.request import urlopen, Request
-from urlib.parse import urlencode
+from urllib.parse import urlencode
 from sys import argv
+
 
 if __name__ == '__main__':
     url = argv[1]
@@ -15,4 +16,5 @@ if __name__ == '__main__':
     utf8data = encodeddata.encode("utf-8")
 
     with urlopen(url, data=utf8data) as response:
+        body = response.read()
         print(response.body.decode("utf-8"))
