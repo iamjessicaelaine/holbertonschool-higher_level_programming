d92f2454e5d83c7dda9957e0a7f601a22ed384f4
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-07-11 20:40:23 -0700

    script for task 6 project 0x0C

---
+++ 0x0C-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""script to post an email using requests module & given url and email"""
+from sys import argv
+import requests
+
+
+if __name__ == '__main__':
+    givenurl = argv[1]
+    givenemail = argv[2]
+    response = requests.post(url, data={'email': givenemail})
+    print(response.text)
