7a4e4ef67f6c9df264284304ce9567dffb6a11c7
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-08-02 21:21:30 -0700

    semistandard correction

--- 0x00-javascript-warm_up/6-multi_languages_loop.js
+++ 0x00-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let j;
 for (j = 0; j < lines.length; j++) {
-    console.log(lines[j]);
+  console.log(lines[j]);
 }
